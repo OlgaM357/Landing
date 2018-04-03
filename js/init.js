d9a59d9ffda4331db65f46f8e8e0e4b1da493ad9
OlgaM357 <stolgam17@gmail.com>
2018-04-03 12:15:12 +0300

    ten commit

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
-'use strict';
- $( function() {
+// 'use strict';
+$( function() {
  	//Init tabs
  	$( "#tabs" ).tabs();
     //Init tabs-portfolio
@@ -11,5 +11,11 @@
  	prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
  	nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
  });
+
+ // Init slick nav
+ $('.header-nav').slicknav({
+ 	appendTo: '.header .container-fluid',//мобильное меню добавили в container-fluid
+	label: ''  //убрали слово меню
+});
 
 });
